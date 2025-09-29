e65d2d5c885812953ba64532f76e734150f4e4ff
shahad <shada-123@hotmail.com>
2025-09-29 10:55:58 +0300

    Initial Commit

---
+++ git1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  git1
+//
+//  Created by shahad khaled on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ git1/git1App.swift
@@ -0,0 +1,17 @@
+//
+//  git1App.swift
+//  git1
+//
+//  Created by shahad khaled on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct git1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
