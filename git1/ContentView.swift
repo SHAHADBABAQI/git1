5093ab9a9b96f654c6f004fab84c26f16545af82
shahad <shada-123@hotmail.com>
2025-09-29 11:40:35 +0300

    Initial Commit

--- git1/ContentView.swift
+++ git1/ContentView.swift
@@ -1,21 +1,41 @@
 //
 //  ContentView.swift
-//  git1
+//  Azm
 //
-//  Created by shahad khaled on 07/04/1447 AH.
+//  Created by shahad khaled on 05/04/1447 AH.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20){
+            
+            Image("Image 6")
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 400, height: 300)
+                .overlay {
+                               Circle().stroke(.yellow, lineWidth: 4)
+                           }
+                           .shadow(radius: 7)
+                           .padding(.top, -100)
+                           
+            VStack(alignment: .leading, spacing: 20){
+                Text("Shahad Khaled Babaqi")
+                    .bold()
+                    .foregroundColor(.orange)
+                    .font(.system(size: 25, weight: .bold, design: .default))
+                
+                Text("i love tennis.")
+                
+            }
+                
+
+            
+            
+            
         }
-        .padding()
     }
 }
 
